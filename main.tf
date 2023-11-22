5dd42a9cb119b6bd6d372c137b6ae64882edba3f
xneyder <xneyder@gmail.com>
2023-11-22 14:45:37 -0500

    fix

--- main.tf
+++ main.tf
@@ -977,20 +977,20 @@ resource "aws_s3_bucket_inventory" "this" {
       prefix     = try(each.value.destination.prefix, null)
 
       dynamic "encryption" {
-        for_each = length(try(flatten([each.value.destination.encryption]), [])) == 0 ? [] : [true]
+        for_each = length(try(each.value.destination.encryption, [])) == 0 ? [] : [true]
 
         content {
 
           dynamic "sse_kms" {
-            for_each = each.value.destination.encryption.encryption_type == "sse_kms" ? [true] : []
+            for_each = each.value.destination.encryption[0].encryption_type == "sse_kms" ? [true] : []
 
             content {
-              key_id = try(each.value.destination.encryption.kms_key_id, null)
+              key_id = try(each.value.destination.encryption[0].kms_key_id, null)
             }
           }
 
           dynamic "sse_s3" {
-            for_each = each.value.destination.encryption.encryption_type == "sse_s3" ? [true] : []
+            for_each = each.value.destination.encryption[0].encryption_type == "sse_s3" ? [true] : []
 
             content {
             }
@@ -1005,10 +1005,10 @@ resource "aws_s3_bucket_inventory" "this" {
   }
 
   dynamic "filter" {
-    for_each = length(try(flatten([each.value.filter]), [])) == 0 ? [] : [true]
+    for_each = length(try(each.value.filter, [])) == 0 ? [] : [true]
 
     content {
-      prefix = try(each.value.filter.prefix, null)
+      prefix = try(each.value.filter[0].prefix, null)
     }
   }
 }
