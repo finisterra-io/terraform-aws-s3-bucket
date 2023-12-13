41b887a033f3dad1e49a72cbb1b5370298f7d4b1
xneyder <xneyder@gmail.com>
2023-12-13 12:06:09 -0500

    Fix

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "s3_bucket_id" {
   value       = try(aws_s3_bucket_policy.this[0].id, aws_s3_bucket.this[0].id, "")
 }
 
-output "s3_bucket_arn" {
+output "arn" {
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
   value       = try(aws_s3_bucket.this[0].arn, "")
 }
